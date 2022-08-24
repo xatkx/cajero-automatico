13a8767cfb056ff4fc92b57ba6f357fb3f198d8c
Abyss <xatkx1@gmail.com>
2022-08-23 20:29:30 -0400

    validacion mas o menos hecha

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ console.log('live')
 
 let dollar = []
 dollar.push({valor: 50, name: 50});
-dollar.push({valor: 20, name: 20});
+dollar.push({valor: 25, name: 20});
 dollar.push({valor: 10, name: 10});
+dollar.push({valor: 5, name: 10});
+dollar.push({valor: 1, name: 10});
+
 
 // let base,base2,bse3;
 
@@ -27,31 +30,63 @@ dollar.push({valor: 10, name: 10});
 
 // console.log('cliente',cliente)
 
-function cajero (cliente){
+function cajero (pide){
     let darCantidad = 0;
     let banco = [
-        {dollar: dollar[0] , cantidad: 3},
-        {dollar: dollar[1], cantidad: 2},
-        {dollar: dollar[2] , cantidad: 2}
+        {dollar: dollar[0] , cantidad: 394},
+        {dollar: dollar[1], cantidad: 299},
+        {dollar: dollar[2] , cantidad: 232},
+        {dollar: dollar[3] , cantidad: 232},
+        {dollar: dollar[4] , cantidad: 232}
+
     ]
+    var dineroTotal = 0
+    banco.forEach( dinero =>{
+        dineroTotal = dineroTotal + dinero.dollar.valor * dinero.cantidad
+    })
+    let personaCliente = [];
 
-    let client22 = [];
-
-    for (let i = 0; i < banco.length ; i++) {
-
-        darCantidad = Math.floor(cliente / banco[i].dollar.valor)
-        
-        if(darCantidad > 0){
-            
-            for (let dar = 0; dar < darCantidad; dar++) {
-                client22.push(dollar[i])
-                
+    if(pide <= 1000){
+        if(!(pide > dineroTotal)){
+            if(!(pide < 10)){
+                for (let i = 0; i < banco.length ; i++) {
+    
+                    darCantidad = Math.floor(pide / banco[i].dollar.valor)
+                    if(darCantidad > 0){
+                        for (let dar = 0; dar < darCantidad; dar++) {
+                            personaCliente.push(dollar[i])
+                        }
+                    }
+                    pide = pide - (darCantidad * banco[i].dollar.valor)
+    
+                    if(i + 1 == banco.length){
+                        return personaCliente
+                    }
+                }
+            } else {
+                console.log('minimo 10 dollar')
             }
-            console.log('cliente',client22)
-            
+        } else {
+            console.log('no tenemos esa cantidad de dollar')
         }
-        cliente = cliente - (darCantidad * banco[i].dollar.valor)  
-        console.log(cliente)
+    } else{
+        console.log('maximo es 1000 dollar')
     }
 }
-cajero(40);
+cajero(180)
+/*
+
+for (let i = 0; i < banco.length ; i++) {
+        darCantidad = Math.floor(pide / banco[i].dollar.valor)
+        if(darCantidad > 0){
+            for (let dar = 0; dar < darCantidad; dar++) {
+                personaCliente.push(dollar[i])
+            }
+        }
+        pide = pide - (darCantidad * banco[i].dollar.valor)  
+        if(i + 1 == banco.length){
+            return personaCliente
+        }
+    }
+
+*/
